ea41c131dd33a6857b2fc0e77f821028f616de03
NoYzE1 <NoYzE@hotmail.de>
2016-02-25 15:36:01 +0100

    Update and rename hashgen.java to Hashgen.java

--- Hashgen.java
+++ Hashgen.java
@@ -1,6 +1,6 @@
 import java.util.Random;
 
-public class Hash {
+public class Hashgen {
 
 	static Random random = new Random();
 	static char[] list = { '0', '1', '2', '3', '4', '5', '6', '7', '8', '9', 'a', 'b', 'c', 'd', 'e', 'f' };
